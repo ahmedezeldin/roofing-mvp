818633ace422cc3cde622efbc17912fbdb7d76b5
ahmedezeldin <ahmedasaad19730@gmail.com>
2026-04-04 21:37:26 -0600

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text, JSON
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text, JSON, func
 from sqlalchemy.orm import relationship
 
 from .db import Base
